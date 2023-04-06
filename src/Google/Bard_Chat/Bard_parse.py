81492715ae55a25fa2b59b46cb7a91ee044c72bb
Lin-Rexter <aaaa35963596@gmail.com>
2023-04-07 02:33:15 +0800

    Rename Bard_Chat.py -> Bard_parse.py

---
+++ src/Google/Bard_Chat/Bard_parse.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+# 讀取.env檔
+from dotenv import load_dotenv
+from Bard import Chatbot
+
+# 讀取.env檔環境變量
+env_path = Path(__file__).resolve().parents[3] / '.env'
+load_dotenv(env_path)
+
+# 獲取Bard的Token
+Bard_Token = os.getenv('BARD_TOKEN', None)
+
+chatbot = Chatbot(Bard_Token)
+
+def Bard_Reply(Texts) -> str:
+    try:
+        return chatbot.ask(Texts)
+    except Exception as e:
+        print("\n", e)
+        return ['Error', "請確認Bard_Token是否正確"]

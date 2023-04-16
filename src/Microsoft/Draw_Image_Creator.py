b5169b362c996667967b0906fc584409c2e0d12d
Lin-Rexter <aaaa35963596@gmail.com>
2023-04-17 05:26:41 +0800

    Fix some problems

--- src/Microsoft/Draw_Image_Creator.py
+++ src/Microsoft/Draw_Image_Creator.py
@@ -8,35 +8,31 @@ from ImageGen import ImageGen
 # take environment variables from .env
 env_path = os.path.join(Path(__file__).resolve().parents[2], '.env')
 
-# check whether .env file exists
-is_env_exist = os.path.exists(env_path) and os.path.isfile(env_path)
+load_dotenv(env_path)
 
-if is_env_exist:
-    load_dotenv(env_path)
-
-    # take AUTH_COOKIE path from environment variables
-    env_auth_cookie = os.getenv('AUTH_COOKIE', None)
+# take AUTH_COOKIE path from environment variables
+env_auth_cookie = os.getenv('AUTH_COOKIE') or None
 
 # take cookies.json path
 cookie_path = os.path.join(Path(__file__).resolve().parents[2], 'cookies.json')
+
+# check whether cookies.json file exists
+is_cookies_exist = os.path.exists(cookie_path) and os.path.isfile(cookie_path)
+
+# Load auth cookie
+if is_cookies_exist:
+    with contextlib.suppress(Exception):
+        with open(cookie_path, encoding="utf-8") as file:
+            cookie_json = json.load(file)
+            for cookie in cookie_json:
+                if cookie.get("name") == "_U":
+                    cookie_U = cookie.get("value")
+                    break
 
 
 async def Image_Creator_Reply(**kwargs) -> list:
     for key, value in kwargs.items():
         globals()[key] = value
-
-    # check whether cookies.json file exists
-    is_cookies_exist = os.path.exists(cookie_path) and os.path.isfile(cookie_path)
-
-    # Load auth cookie
-    if is_cookies_exist:
-        with contextlib.suppress(Exception):
-            with open(cookie_path, encoding="utf-8") as file:
-                cookie_json = json.load(file)
-                for cookie in cookie_json:
-                    if cookie.get("name") == "_U":
-                        cookie_U = cookie.get("value")
-                        break
 
     # auth cookie
     U = auth_cookies or cookie_U or env_auth_cookie or None

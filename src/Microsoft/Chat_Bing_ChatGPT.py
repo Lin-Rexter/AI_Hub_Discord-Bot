6918e7c43cf0ac803153f71f711f243a325143eb
Lin-Rexter <aaaa35963596@gmail.com>
2023-04-17 05:26:18 +0800

    Fix some problems

--- src/Microsoft/Chat_Bing_ChatGPT.py
+++ src/Microsoft/Chat_Bing_ChatGPT.py
@@ -1,14 +1,21 @@
 import os
 import sys
 import contextlib
+from dotenv import load_dotenv
 from pathlib import Path
 from EdgeGPT import Chatbot, ConversationStyle
 
 # take cookies.json path
 cookie_path = os.path.join(Path(__file__).resolve().parents[2], 'cookies.json')
 
-# check whether cookies.json file exists
-is_exist = os.path.exists(cookie_path) and os.path.isfile(cookie_path)
+env_cookies = os.getenv('BING_CHAT_COOKIES') or None
+
+# take environment variables from .env
+env_path = os.path.join(Path(__file__).resolve().parents[2],'.env')
+
+load_dotenv(env_path)
+
+env_response_style = os.getenv('RESPONSE_STYLE') or None
 
 '''
 if not is_exist:
@@ -21,20 +28,34 @@ if os.path.getsize(cookie_path) <= 0:
 # https://docs.python.org/zh-cn/3.8/library/contextlib.html#contextlib.suppress
 # 錯誤抑制
 with contextlib.suppress(Exception):
-    bot = Chatbot(cookiePath=cookie_path)
+    bot = Chatbot(cookiePath = cookie_path or env_cookies)
 
 async def EdgeGPT_Reply(**kwargs) -> list:
     try:
         for key, value in kwargs.items():
             globals()[key] = value
 
+        Translator_Style = {
+            "CREATIVE": "創意",
+            "BALANCED": "平衡",
+            "PRECISE": "精確"
+        }
+
         Response_Style = {
             "創意": ConversationStyle.creative,
             "平衡": ConversationStyle.balanced,
             "精確": ConversationStyle.precise
-        } 
+        }
+        
+        choice_style = style_name or None
 
-        style = Response_Style[style_name]
+        if choice_style is None:
+            if env_response_style not in Translator_Style.keys():
+                choice_style = "平衡"
+            else:
+                choice_style = Translator_Style[env_response_style.upper()]
+
+        style = Response_Style[choice_style]
 
         reply_dict = await bot.ask(prompt=prompts, conversation_style=style)
         reply = reply_dict["item"]["messages"][1]["text"]
